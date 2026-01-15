7e82c2a48c6eef3cd99894c261e05d80be0c8b98
Eduardo Rojas <carlos.edhr@protonmail.com>
2026-01-14 23:45:38 -0800

    changed frontend_url settings

--- backend/settings.py
+++ backend/settings.py
@@ -150,10 +150,11 @@ CORS_ALLOWED_ORIGINS = [
     "https://127.0.0.1:8000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://frontend-bitspirals.pages.dev",
 ]
 
 # frontend domain, add  here
-FRONTEND_URL = os.environ.get("https://frontend-bitspirals.pages.dev", "")
+FRONTEND_URL = os.environ.get("FRONTEND_URL", "")
 if FRONTEND_URL:
     CORS_ALLOWED_ORIGINS.append(FRONTEND_URL)
 
